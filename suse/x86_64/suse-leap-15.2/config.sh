5bc1b3f161daaceccee833b235815a1800eba8ef
Christian Goll <cgoll@suse.de>
2021-02-17 15:25:57 +0100

    make leap15.2 more consistens

--- suse/x86_64/suse-leap-15.2/config.sh
+++ suse/x86_64/suse-leap-15.2/config.sh
@@ -32,6 +32,11 @@ echo "Configure image: [$kiwi_iname]..."
 suseSetupProduct
 
 #======================================
+# Add missing gpg keys to rpm
+#--------------------------------------
+suseImportBuildKey
+
+#======================================
 # Activate services
 #--------------------------------------
 suseInsertService sshd
@@ -40,3 +45,32 @@ suseInsertService sshd
 # Setup default target, multi-user
 #--------------------------------------
 baseSetRunlevel 3
+
+
+#======================================
+# only basic version of vim is
+# installed; no syntax highlighting
+#--------------------------------------
+sed -i -e's/^syntax on/" syntax on/' /etc/vimrc
+
+#======================================
+# SuSEconfig
+#--------------------------------------
+suseConfig
+
+#======================================
+# Remove yast if not in use
+#--------------------------------------
+suseRemoveYaST
+
+# add local repo
+mkdir /root/local
+zypper ar -p10 /root/local/ local
+
+# have a seperate user
+useradd -m chris
+cp -r /root/.ssh /home/chris
+chown chris:users /home/chris/.ssh
+groupadd -r munge
+useradd -r -g munge -d /run/munge -s /bin/false -c "MUNGE user" munge
+chown -R munge:munge /etc/munge
