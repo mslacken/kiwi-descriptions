1058636c9e42a48dec3b0ab0c134ca48612a17b6
Christian Goll <cgoll@suse.de>
2021-03-15 13:44:18 +0100

    added 15.3

---
+++ suse/x86_64/suse-leap-15.3/config.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#================
+# FILE          : config.sh
+#----------------
+# PROJECT       : OpenSuSE KIWI Image System
+# COPYRIGHT     : (c) 2006 SUSE LINUX Products GmbH. All rights reserved
+#               :
+# AUTHOR        : Marcus Schaefer <ms@suse.de>
+#               :
+# BELONGS TO    : Operating System images
+#               :
+# DESCRIPTION   : configuration script for SUSE based
+#               : operating systems
+#               :
+#               :
+# STATUS        : BETA
+#----------------
+#======================================
+# Functions...
+#--------------------------------------
+test -f /.kconfig && . /.kconfig
+test -f /.profile && . /.profile
+
+#======================================
+# Greeting...
+#--------------------------------------
+echo "Configure image: [$kiwi_iname]..."
+
+#======================================
+# Setup baseproduct link
+#--------------------------------------
+suseSetupProduct
+
+#======================================
+# Add missing gpg keys to rpm
+#--------------------------------------
+suseImportBuildKey
+
+#======================================
+# Activate services
+#--------------------------------------
+suseInsertService sshd
+
+#======================================
+# Setup default target, multi-user
+#--------------------------------------
+baseSetRunlevel 3
+
+
+#======================================
+# only basic version of vim is
+# installed; no syntax highlighting
+#--------------------------------------
+sed -i -e's/^syntax on/" syntax on/' /etc/vimrc
+
+#======================================
+# SuSEconfig
+#--------------------------------------
+suseConfig
+
+#======================================
+# Remove yast if not in use
+#--------------------------------------
+suseRemoveYaST
+
+# add local repo
+mkdir /root/local
+zypper ar -p10 /root/local/ local
+
+cp -r /root/.ssh /home/chris
+chown chris:users /home/chris/.ssh
+#groupadd -r munge
+#useradd -r -g munge -d /run/munge -s /bin/false -c "MUNGE user" munge
+#chown -R munge:munge /etc/munge
